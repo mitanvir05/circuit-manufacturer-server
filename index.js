5013204055dac8190bfc897143fc07c8e166b715
mitanvir05 <tanvir.cse05@gmail.com>
2022-05-26 00:45:01 +0600

    initial setup done

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+
 require("dotenv").config();
+const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Tools Loading");
+});
+app.listen(port, () => {
+  console.log("Listening port", port);
+});
